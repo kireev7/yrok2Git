ea121cbae738f71d118c60ec7102808b1be22b4d
Sergiy2 <jssk303@gmail.com>
2025-07-03 12:16:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char const *argv[])
 {
     std::cout << "hello world" << std::endl;
     std::cout << "git pull" << std::endl;
+    std::cout << "2 + 3 = " << 2 + 3 << std::endl;
     return 0;
 }
